13e8b5253ac7403e93f7642e672a9ff2099ecdec
Jo <Jlee.naidoo@gmail.com>
2022-10-04 14:51:50 +0200

    Sci_Fi_Demo

---
+++ Scripts/Destructable.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _crateDestroyed;
+
+    public void DestroyCrate()
+    {
+        Instantiate(_crateDestroyed, transform.position, transform.rotation);
+        Destroy(this.gameObject);
+    }
+
+}
